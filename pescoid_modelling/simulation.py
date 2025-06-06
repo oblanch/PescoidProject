Fixed measurement of mesoderm fraction, Created new measuremnt for mesoderm average Updated simulation_logger.py to incorporate and save new metrics

pescoid_modelling/simulation.py
@@ -149,6 +149,7 @@ class PescoidSimulator:
     def _initialize_simulation_and_solver(self) -> None:
         """Set up the simulation environment."""
         self._setup_mesh()
+        self._get_half_domain_idx()
         self._initialize_logger()
         self._setup_function_spaces()
         self._set_initial_conditions()
@@ -164,6 +165,11 @@ class PescoidSimulator:
         self._mesh = IntervalMesh(
             num_mesh_points, -half_domain_length, half_domain_length
         )
+    def _get_half_domain_idx(self) -> int:
+        """Get the index at the center of the mesh"""
+        domain_length = getattr(self.params, "domain_length", 10.0)
+        mesh_spacing = getattr(self.params, "dx_interval", 5e-3)
+        self.half_domain_idx = int(np.round((domain_length/2)/mesh_spacing))
 
     def _initialize_logger(self) -> None:
         """Initialize the simulation logger to record results."""
@@ -560,6 +566,36 @@ class PescoidSimulator:
             edge_idx = len(x_coords) - 1
         return edge_x, edge_idx
 
+    def _compute_mesoderm_fraction(
+            self, m_vals: np.array, edge_idx: float
+    ) -> float:
+        '''Calculate the fraction of tissue that is expressing mesoderm
+        '''
+        
+        m_pos = []
+        m_neg = []
+        m_frac = []
+        i_range = None
+
+        start_index = self.half_domain_idx
+        if edge_idx is not None:
+            i_range = range(start_index,edge_idx)
+            for i in i_range:
+                if m_vals[i] > 0:
+                    m_pos.append(m_vals[i])  # Use append to add elements to the list
+                else:
+                    m_neg.append(m_vals[i])
+            m_frac = len(m_pos) / (len(m_neg)+len(m_pos))
+        return m_frac
+        
+    def _compute_mesoderm_average(
+            self, m_vals: np.array, edge_idx: int
+    ) -> float:
+        """ Takes average mesoderm values from everywhere within the tissue"""
+        meso_all = m_vals[self.half_domain_idx:edge_idx+1]
+        meso_avg = meso_all.mean()
+        return meso_avg
+
     def _radius_norm(self, edge_x: float) -> float:
         """Calculate the normalized area based on the leading edge position.
 
@@ -572,12 +608,14 @@ class PescoidSimulator:
         rho_fn, m_fn, u_fn = self._current_state.split()
         rho_vals = rho_fn.compute_vertex_values(self._mesh)
         m_vals = m_fn.compute_vertex_values(self._mesh)
+        m_center = m_vals[self.half_domain_idx]
         u_vals = u_fn.compute_vertex_values(self._mesh)
         x_coords = self._mesh.coordinates().flatten()
         max_m = m_vals.max()
-        meso_frac = (m_vals > 0).mean()
 
         edge_x, edge_idx = self._compute_leading_edge(rho_vals, x_coords)
+        meso_frac = self._compute_mesoderm_fraction(m_vals, edge_idx)
+        m_avg = self._compute_mesoderm_average(m_vals, edge_idx)
         stress_vals = self._compute_stress(rho_fn, m_fn)
         radius_star = self._radius_norm(edge_x)
 
@@ -586,6 +624,8 @@ class PescoidSimulator:
             current_time=current_time,
             rho_vals=rho_vals,
             m_vals=m_vals,
+            m_center = m_center,
+            m_avg= m_avg,
             u_vals=u_vals,
             stress_vals=stress_vals,
             edge_x=edge_x,